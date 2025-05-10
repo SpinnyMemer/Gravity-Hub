21eee93198f02e2aa1e96f98ad4c19d8b7d9ffa2
SpinnyMemer <110652422+SpinnyMemer@users.noreply.github.com>
2025-05-10 17:55:48 +0530

    Delete Gravity.lua

--- Gravity.lua
+++
@@ -1,5 +0,0 @@
-if game.PlaceId == 286090429 then
-   loadstring(game:HttpGet('https://raw.githubusercontent.com/SpinnyMemer/Gravity-Hub/refs/heads/main/Arsenal.lua'))()
-else
-   print("Game not supported.")
-end
