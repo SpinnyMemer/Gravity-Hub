d6001ecc67bd1c3514cd2047c56c018d8bb8d930
SpinnyMemer <110652422+SpinnyMemer@users.noreply.github.com>
2025-05-10 17:56:50 +0530

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 286090429 then
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/SpinnyMemer/Gravity-Hub/refs/heads/main/ArsenalNew.lua'))()
+else
+   print("Game not supported.")
+end
