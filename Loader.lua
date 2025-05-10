57297a4c83142d5b07f650156ba1fc73690ab706
SpinnyMemer <110652422+SpinnyMemer@users.noreply.github.com>
2025-05-11 02:42:40 +0530

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == 286090429 then
    loadstring(game:HttpGet('https://raw.githubusercontent.com/SpinnyMemer/Gravity-Hub/refs/heads/main/ArsenalNew.lua'))()
+elseif game.PlaceId == 17625359962 then
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/SpinnyMemer/Gravity-Hub/refs/heads/main/Rivals.lua'))()
 else
-   print("Game not supported.")
-end
+   print("Game not supported!")
+end   
