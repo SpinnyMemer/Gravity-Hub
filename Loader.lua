1fbcbd947fbe9202177a634fe4f0d0a5522e9eba
SpinnyMemer <110652422+SpinnyMemer@users.noreply.github.com>
2025-05-12 07:57:57 +0530

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -10,6 +10,8 @@ elseif game.PlaceId == 17625359962 then
    loadstring(game:HttpGet('https://raw.githubusercontent.com/SpinnyMemer/Gravity-Hub/refs/heads/main/Rivals.lua'))()
 elseif game.PlaceId == 101435587895051 or 9441238005 or 17856909502 or 8255927517 then
    loadstring(game:HttpGet('https://raw.githubusercontent.com/SpinnyMemer/Gravity-Hub/refs/heads/main/Aimblox.lua'))()
+elseif game.PlaceId = 893973440 then
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/SpinnyMemer/Gravity-Hub/refs/heads/main/FTF.lua'))()
 else
    print("Game not supported :/")
 end
